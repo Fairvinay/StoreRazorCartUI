8fe47699b8b1df85705d510cb3457c5c24a53d26
Fairvinay <fairvinay@gmail.com>
2025-05-17 03:20:51 +0530

    separate config js hardcodes to handle env production seems override the Choreo Devops Configs and Secrets

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
-export const API_URL = process.env.REACT_APP_SERVER_URL || "https://23f24711-ee18-4810-935b-61f010c33bec.e1-us-east-azure.choreoapps.dev";
+export const API_URL =   "https://23f24711-ee18-4810-935b-61f010c33bec.e1-us-east-azure.choreoapps.dev";
+//export const API_URL = process.env.REACT_APP_SERVER_URL || "https://23f24711-ee18-4810-935b-61f010c33bec.e1-us-east-azure.choreoapps.dev";
 
 
 /*
