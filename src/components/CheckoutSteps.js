3a399989c03e921ba84fa6b19e24dd76d09422c6
Fairvinay <fairvinay@gmail.com>
2025-05-20 08:09:26 +0530

    header.css and shipping CheckoutSteps bottom margin reduction to make continue button visible

--- src/components/CheckoutSteps.js
+++ src/components/CheckoutSteps.js
@@ -1,9 +1,10 @@
 import { Nav } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 
-const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
+const CheckoutSteps = ({ step1, step2, step3, step4 }) => { 
+   //  className="justify-content-center  mb-4
   return (
-    <Nav className="justify-content-center mb-4">
+    <Nav className="justify-content-center"> 
       <Nav.Item>
         {step1 ? (
           <LinkContainer to="/login">
